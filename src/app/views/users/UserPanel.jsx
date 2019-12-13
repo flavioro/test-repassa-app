0b27533805f8443b2a7f8319c06add1a21ea91a8
i.espinola <ivan.espinola@live.com>
2019-12-13 00:01:10 -0300

    readme

--- src/app/views/users/UserPanel.jsx
+++ src/app/views/users/UserPanel.jsx
@@ -58,6 +58,7 @@ class UserPanel extends React.Component
 
     componentDidMount = () =>
     {
+        console.log(this.props.history)
         if (!this.props.data.user.id) this.props.history.push('/user')
     }
 
